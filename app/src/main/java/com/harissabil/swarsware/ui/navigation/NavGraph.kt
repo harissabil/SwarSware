b4664ba8dc72f379269e4312cb6d36c166f2e4cb
Haris <harissabil04@gmail.com>
2025-03-26 13:11:43 +0700

    Add navigation for Onboarding and Home screens in NavGraph

--- app/src/main/java/com/harissabil/swarsware/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/harissabil/swarsware/ui/navigation/NavGraph.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.harissabil.swarsware.ui.screen.home.HomeScreen
+import com.harissabil.swarsware.ui.screen.onboarding.OnboardingScreen
 
 @Composable
 fun NavGraph(
@@ -18,9 +20,22 @@ fun NavGraph(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable<Route.Onboarding> { }
-        composable<Route.Home> { }
+        composable<Route.Onboarding> {
+            OnboardingScreen(
+                onGetStartedClicked = {
+                    navController.navigate(Route.Home) {
+                        popUpTo(Route.Onboarding) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable<Route.Home> {
+            HomeScreen()
+        }
+
         composable<Route.Sounds> { }
+
         composable<Route.Emergency> { }
     }
 }
