45e8240b193102f862783c16b22571df46fd2ece
Haris <harissabil04@gmail.com>
2025-03-27 17:15:03 +0700

    Add HomeViewModel to manage timer status and elapsed time

--- app/src/main/java/com/harissabil/swarsware/di/ViewModelModule.kt
+++ app/src/main/java/com/harissabil/swarsware/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.harissabil.swarsware.di
 
 import com.harissabil.swarsware.MainViewModel
+import com.harissabil.swarsware.ui.screen.home.component.HomeViewModel
 import com.harissabil.swarsware.ui.screen.onboarding.OnboardingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModelOf(::MainViewModel)
     viewModelOf(::OnboardingViewModel)
+    viewModelOf(::HomeViewModel)
 }
